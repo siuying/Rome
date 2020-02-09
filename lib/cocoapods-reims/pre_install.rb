fdfb6d67526d3019a02444d18c93867bd8b510ee
Francis Chong <siu.ying@gmail.com>
2020-02-09 13:03:47 +0800

    no need use module headers

--- lib/cocoapods-reims/pre_install.rb
+++ lib/cocoapods-reims/pre_install.rb
@@ -1,6 +1,5 @@
 Pod::HooksManager.register('cocoapods-reims', :pre_install) do |installer_context|
   podfile = installer_context.podfile
-  podfile.use_modular_headers!
   podfile.install!('cocoapods',
     podfile.installation_method.last.merge(:integrate_targets => false))
 end
